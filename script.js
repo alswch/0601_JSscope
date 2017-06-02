69bc3e55ed3c2b0e011886c1c2a214945dfbeb6c
alswch <alswchung@gmail.com>
2017-06-02 10:14:44 -0400

    in-progress: working on keypress function

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 // ====== arguing buttons ======
-document.getElementById("button_1").addEventListener("click", showMsg);
-function showMsg() {
+var button1 = document.getElementById('button_1');
+var button2 = document.getElementById('button_2');
+
+button1.addEventListener("click", function() {
   var textDisplay = document.getElementById("textDisplay");
   textDisplay.innerHTML = "I'm right"
-};
-document.getElementById("button_2").addEventListener("click", showMsg2);
-function showMsg2() {
+})
+
+button2.addEventListener("click", function() {
   var textDisplay = document.getElementById("textDisplay");
-  textDisplay.innerHTML = "No, I'm right"
-};
+  textDisplay.innerHTML = "No, I'm right!"
+})
 
 // ====== don't hover over me element  ======
-document.getElementById("dont").addEventListener("mouseover", function(){
-    alert("Hey, I told you not to hover over me!");
-});
+var dont = document.getElementById('dont');
+
+dont.addEventListener("mouseover", function() {
+  alert("Hey, I told you not to hover over me!");
+})
+
+// // ====== keypress display  ======
+// document.getElementById("keypress").addEventListener("keypress", myKeypress);
+//
+// function myKeypress() {
+//     document.getElementById("text") =
+// }
+var keyPress = document.getElementById('keys');
+
+keyPress.addEventListener("keypress", function() {
+  keyPress.innerHTML = " "
+})
